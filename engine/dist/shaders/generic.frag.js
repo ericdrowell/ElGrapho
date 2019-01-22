45ac7924151bf4c4fc62a846915590f8531753a0
Eric Rowell <ericrowell@Erics-MacBook-Pro.local>
2019-01-21 21:57:14 -0800

    cleanup dist

--- engine/dist/shaders/generic.frag.js
+++
@@ -1,8 +0,0 @@
-module.exports = `// use lowp for solid colors to improve perf
-// https://stackoverflow.com/questions/13780609/what-does-precision-mediump-float-mean
-precision lowp float;
-varying vec4 vVertexColor;
-
-void main(void) {
-  gl_FragColor = vVertexColor;
-}`;
