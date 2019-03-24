b8a869349dfcbfa9f128fca21dcb60b4fb288fcf
Eric Rowell <ericrowell@Erics-MacBook-Pro.local>
2019-03-23 23:29:26 -0700

    finished updating all of the examples with new API

--- engine/dist/ElGrapho.js
+++ engine/dist/ElGrapho.js
@@ -3179,125 +3179,55 @@ module.exports = Cluster;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-// const glMatrix = require('gl-matrix');
-// const vec2 = glMatrix.vec2;
 const fitToViewport = __webpack_require__(/*! ./utils/fitToViewport */ "./engine/src/models/utils/fitToViewport.js");
-const DEFAULT_STEPS = 10;
+const DEFAULT_STEPS = 20;
 const POSITION_FACTOR = 0.3;
-//const REPEL_FACTOR = 0.01;
-//const REPEL_FROM_CENTER_FACTOR = 0;
 const ATTRACT_FACTOR = 0.1;
 
 const initNodePositions = function(nodes) {
-  let numNodes = nodes.colors.length;
+  let numNodes = nodes.length;
 
-  // find color counts
-  let colors = [];
-  for (let a=0; a<numNodes; a++) {
-    let color = nodes.colors[a];
+  // find group counts
+  let groups = [];
+  nodes.forEach(function(node) {
+    let group = node.group;
 
-    if (colors[color] === undefined) {
-      colors[color] = {
+    if (groups[group] === undefined) {
+      groups[group] = {
         count: 0
       };
     }
 
-    colors[color].count++;
-  }
+    groups[group].count++;
+  });
 
   let total = 0;
-  for (let n=0; n<colors.length; n++) {
-    colors[n].next = total;
-    total+=colors[n].count;
+  for (let n=0; n<groups.length; n++) {
+    groups[n].next = total;
+    total+=groups[n].count;
   }
 
   // initialize positions
-  for (let a=0; a<numNodes; a++) {
-    let color = nodes.colors[a];
-    let angle = -2 * Math.PI * colors[color].next++ / numNodes;
-    //let angle = -2 * Math.PI * a / numNodes;
+  nodes.forEach(function(node) {
+    let group = node.group;
+    let angle = -2 * Math.PI * groups[group].next++ / numNodes;
 
-    nodes.xs[a] = POSITION_FACTOR * Math.cos(angle);
-    nodes.ys[a] = POSITION_FACTOR * Math.sin(angle);
-  }
+    node.x = POSITION_FACTOR * Math.cos(angle);
+    node.y = POSITION_FACTOR * Math.sin(angle);
+  });
 };
-
-// repulsive forces for all nodes
-// Coulomb's Law -> F = q1 * q2 / d^2
-// const repelNodesFromEachOther = function(nodes) {
-//   let numNodes = nodes.colors.length;
-//   let xChanges = [];
-//   let yChanges = [];
-
-//   for (let a=0; a<numNodes; a++) {
-//     xChanges[a] = 0;
-//     yChanges[a] = 0;
-
-//     for (let b=0; b<numNodes; b++) {
-//       let ax = nodes.xs[a];
-//       let ay = nodes.ys[a];
-//       let bx = nodes.xs[b];
-//       let by = nodes.ys[b];
-//       let xDiff = bx - ax;
-//       let yDiff = by - ay;
-//       let dist = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
-
-//       if (dist > 0) {
-//         // move a away from b
-//         let xChange = -1 * REPEL_FACTOR * xDiff / (dist * dist);
-//         let yChange = -1 * REPEL_FACTOR * yDiff / (dist * dist);
-
-//         xChanges[a] += xChange;
-//         yChanges[a] += yChange;
-//       }
-//     }
-//   }
-//   //update node positions for repulsive forces
-//   for (let i=0; i<numNodes; i++) {
-//     nodes.xs[i] += xChanges[i];
-//     nodes.ys[i] += yChanges[i];
-//   }
-// };
-
-// const repelNodesFromCenter = function(nodes) {
-//   let numNodes = nodes.colors.length;
-
-//   let ax = 0;
-//   let ay = 0;
-
-//   for (let b=0; b<numNodes; b++) {
-//     let bx = nodes.xs[b];
-//     let by = nodes.ys[b];
-//     let xDiff = bx - ax;
-//     let yDiff = by - ay;
-
-//     let vector = vec2.fromValues(xDiff, yDiff);
-//     let normalizedVector = vec2.normalize(vec2.create(), vector);
-//     let changeVector = vec2.scale(vec2.create(), normalizedVector, REPEL_FROM_CENTER_FACTOR);
-
-
-//     nodes.xs[b] += changeVector[0];
-//     nodes.ys[b] += changeVector[1];
-    
-    
-//   }
-// };
 
 // attractive forces between nodes sharing an edge
 // Hooke's Law -> F = kx
 const attractNodes = function(nodes, edges) {
-  let numEdges = edges.from.length;
+  edges.forEach(function(edge) {
+    let a = edge.from;
+    let b = edge.to;
 
-  for (let i=0; i<numEdges; i++) {
-    let a = edges.from[i];
-    let b = edges.to[i];
-
-    let ax = nodes.xs[a];
-    let ay = nodes.ys[a];
-    let bx = nodes.xs[b];
-    let by = nodes.ys[b];
-    // let aColor = nodes.colors[a];
-    // let bColor = nodes.colors[b];
+    let ax = nodes[a].x;
+    let ay = nodes[a].y;
+    let bx = nodes[b].x;
+    let by = nodes[b].y;
     let xDiff = bx - ax;
     let yDiff = by - ay;
     let dist = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
@@ -3309,41 +3239,18 @@ const attractNodes = function(nodes, edges) {
       yChange = ATTRACT_FACTOR * yDiff;
 
       // move a closer to b
-      nodes.xs[a] += xChange;
-      nodes.ys[a] += yChange;
+      nodes[a].x += xChange;
+      nodes[a].y += yChange;
 
       // move b closer to a
-      nodes.xs[b] -= xChange;
-      nodes.ys[b] -= yChange;
+      nodes[b].x -= xChange;
+      nodes[b].y -= yChange;
     }
-  }
+  });
 };
 
-const ForceDirectedGraph = function(config) {
-  let numNodes = config.nodes.colors.length;
-  let steps = config.steps === undefined ? DEFAULT_STEPS : config.steps;
-
-  let model = {
-    nodes: {
-      xs: [],
-      ys: [],
-      colors: config.nodes.colors.slice()
-    },
-    edges: {
-      from: config.edges.from.slice(),
-      to: config.edges.to.slice()
-    },
-    width: config.width,
-    height: config.height,
-    steps: steps
-  };
-
-  model.nodes.xs.length = numNodes;
-  model.nodes.xs.fill(0);
-
-  model.nodes.ys.length = numNodes;
-  model.nodes.ys.fill(0);
-
+const ForceDirectedGraph = function(model) {
+  let steps = model.steps === undefined ? DEFAULT_STEPS : model.steps;
   let nodes = model.nodes;
   let edges = model.edges;
 
@@ -3351,8 +3258,6 @@ const ForceDirectedGraph = function(config) {
 
   // process steps
   for (let n=1; n<steps; n++) {
-    //repelNodesFromEachOther(nodes);
-    //repelNodesFromCenter(nodes);
     attractNodes(nodes, edges);
   }
 
